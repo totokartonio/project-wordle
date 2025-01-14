1811065d1599bcc884ac5945d57482e77562c459
Anton Efimchenko <efimchenkoa@MacBook-Pro-Anton.local>
2025-01-14 22:39:37 +0100

    Add guess input

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function GuessInput() {
+  const [word, setWord] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({ word });
+
+    setWord("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={word}
+        onChange={(event) => {
+          newWord = event.target.value;
+          newWord = newWord.toUpperCase();
+          setWord(newWord);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
